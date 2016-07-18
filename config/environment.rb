51665764c94e8910e47b79c4b30abcd8ab87df79
rmnlknv <snovix@gmail.com>
2016-07-18 03:50:51 +0200

    Added actionmailer settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'gmail.com',
+      authentication: 'plain',
+      enable_starttls_auto: true,
+      user_name: 'lisiykolya@gmail.com',
+      password: 'cdzabtszuvepuinc'
+}
